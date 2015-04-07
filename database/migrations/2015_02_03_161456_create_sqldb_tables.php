88fcbbbee5f95e62d46b57c0d64d3332978b3e6b
Arif Islam <arif@dreamfactory.com>
2015-04-07 17:29:41 -0400

    updating service table name

--- database/migrations/2015_02_03_161456_create_sqldb_tables.php
+++ database/migrations/2015_02_03_161456_create_sqldb_tables.php
@@ -18,7 +18,7 @@ class CreateSqlDbTables extends Migration
             function ( Blueprint $t )
             {
                 $t->integer( 'service_id' )->unsigned()->primary();
-                $t->foreign( 'service_id' )->references( 'id' )->on( 'services' )->onDelete( 'cascade' );
+                $t->foreign( 'service_id' )->references( 'id' )->on( 'service' )->onDelete( 'cascade' );
                 $t->string( 'dsn' )->default( 0 );
                 $t->string( 'username' )->nullable();
                 $t->string( 'password' )->nullable();
