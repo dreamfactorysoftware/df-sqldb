78a20e48337355ee67aaf0f8be7b334da0628190
Tomo Norman <tomo.l.norman@gmail.com>
2022-07-04 20:59:47 +0900

    DP-535 Fix comment classes

--- src/Database/Schema/AlloyDbSchema.php
+++ src/Database/Schema/AlloyDbSchema.php
@@ -3,7 +3,7 @@
 namespace DreamFactory\Core\SqlDb\Database\Schema;
 
 /**
- * Schema is the class for retrieving metadata information from a PostgreSQL database.
+ * Schema is the class for retrieving metadata information from an AlloyDB database.
  */
 class AlloyDbSchema extends PostgresSchema
 {
